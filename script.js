8500cd261e0f23834c60a4c549455c01f1c76f67
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-04-12 16:19:19 +0100

    add pagination in step bar

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("hello");
+const step1 = document.querySelector("#step_1");
+const step2 = document.querySelector("#step_2");
+const step3 = document.querySelector("#step_3");
+const step4 = document.querySelector("#step_4");
+
+const product_picture = document.querySelector(".main_product_left");
+const product_info = document.querySelector(".product_information");
+const profile_form = document.querySelector(".profile-form");
+const address_form = document.querySelector(".address-form ");
+const shipping_form = document.querySelector(".shipping-form");
+const finish_form = document.querySelector(".finish");
+
+step1.addEventListener("click", () => {
+  console.log("step1");
+  product_info.classList.add("hidden");
+  profile_form.classList.remove("hidden");
+});
+step2.addEventListener("click", () => {
+  console.log("step2");
+  profile_form.classList.add("hidden");
+  address_form.classList.remove("hidden");
+  console.log(address_form.className);
+});
+step3.addEventListener("click", () => {
+  console.log("step3");
+  profile_form.classList.add("hidden");
+  address_form.classList.add("hidden");
+  shipping_form.classList.remove("hidden");
+  finish_form.classList.add("hidden");
+});
+step4.addEventListener("click", () => {
+  console.log("step4");
+  profile_form.classList.add("hidden");
+  address_form.classList.add("hidden");
+  shipping_form.classList.add("hidden");
+  finish_form.classList.remove("hidden");
+});
