ec88dda9d7cff62391f69a3e5c434c343d358501
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-04-12 17:02:00 +0100

    implement next page functionality

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const step1 = document.querySelector("#step_1");
 const step2 = document.querySelector("#step_2");
 const step3 = document.querySelector("#step_3");
 const step4 = document.querySelector("#step_4");
+const buy_button = document.getElementById("buy_button");
+
+const main_product_right = document.querySelector(".main_product_right");
+const next_button_forms =
+  main_product_right.querySelectorAll(".next_button_form");
 
 const product_picture = document.querySelector(".main_product_left");
 const product_info = document.querySelector(".product_information");
@@ -9,6 +14,23 @@ const profile_form = document.querySelector(".profile-form");
 const address_form = document.querySelector(".address-form ");
 const shipping_form = document.querySelector(".shipping-form");
 const finish_form = document.querySelector(".finish");
+
+buy_button.addEventListener("click", () => {
+  product_info.classList.add("hidden");
+  profile_form.classList.remove("hidden");
+});
+
+Array.from(next_button_forms).forEach((button) => {
+  button.addEventListener("click", (event) => {
+    next_form(event);
+  });
+});
+
+function next_form(event) {
+  const sectionForm = event.target.parentElement.parentElement;
+  sectionForm.classList.add("hidden");
+  sectionForm.nextElementSibling.classList.remove("hidden");
+}
 
 step1.addEventListener("click", () => {
   console.log("step1");
